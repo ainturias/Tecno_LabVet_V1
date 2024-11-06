2bb754686f162568e0e2acf614a2d2a1e36769bb
ainturias <alex.universidad18@gmail.com>
2024-11-05 20:58:07 -0400

    Creacion del proyecto

---
+++ src/main/java/com/mycompany/tecno_labvet_v1/Tecno_LabVet_V1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tecno_labvet_v1;
+
+/**
+ *
+ * @author aintu
+ */
+public class Tecno_LabVet_V1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
